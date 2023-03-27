532ce9459390fc49e4b3ce8bdb17beb6d1e6877f
Hubert-ubuntu <12012702@mail.sustech.edu.cn>
2023-03-27 17:51:58 +0800

    checkstyle2

--- src/OnlineCoursesAnalyzer.java
+++ src/OnlineCoursesAnalyzer.java
@@ -9,8 +9,8 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;    
-       
+import java.util.Map;
+
 
 /**
  * This is just a demo for you, please run it on JDK17
@@ -19,9 +19,14 @@ import java.util.Map;
  * based on this demo, or implement it in a different way.
  */
 public class OnlineCoursesAnalyzer {
-    
-    List<Course> courses = new ArrayList<>();
 
+    List<Course> courses = new ArrayList<>();
+    /**
+     * This is a method description that is long enough to exceed right margin.
+     * Line with manual
+     * line feed.
+     *
+     */
     public OnlineCoursesAnalyzer(String datasetPath) {
         BufferedReader br = null;
         String line;
@@ -30,13 +35,25 @@ public class OnlineCoursesAnalyzer {
             br.readLine();
             while ((line = br.readLine()) != null) {
                 String[] info = line.split(",(?=([^\\\"]*\\\"[^\\\"]*\\\")*[^\\\"]*$)", -1);
-                Course course = new Course(info[0], info[1], new Date(info[2]), info[3], info[4], info[5],
-                        Integer.parseInt(info[6]), Integer.parseInt(info[7]), Integer.parseInt(info[8]),
-                        Integer.parseInt(info[9]), Integer.parseInt(info[10]), Double.parseDouble(info[11]),
-                        Double.parseDouble(info[12]), Double.parseDouble(info[13]), Double.parseDouble(info[14]),
-                        Double.parseDouble(info[15]), Double.parseDouble(info[16]), Double.parseDouble(info[17]),
-                        Double.parseDouble(info[18]), Double.parseDouble(info[19]), Double.parseDouble(info[20]),
-                        Double.parseDouble(info[21]), Double.parseDouble(info[22]));
+                Course course = new Course(info[0], info[1], new Date(info[2]),
+                        info[3], info[4], info[5],
+                        Integer.parseInt(info[6]),
+                        Integer.parseInt(info[7]),
+                        Integer.parseInt(info[8]),
+                        Integer.parseInt(info[9]),
+                        Integer.parseInt(info[10]),
+                        Double.parseDouble(info[11]),
+                        Double.parseDouble(info[12]),
+                        Double.parseDouble(info[13]),
+                        Double.parseDouble(info[14]),
+                        Double.parseDouble(info[15]),
+                        Double.parseDouble(info[16]),
+                        Double.parseDouble(info[17]),
+                        Double.parseDouble(info[18]),
+                        Double.parseDouble(info[19]),
+                        Double.parseDouble(info[20]),
+                        Double.parseDouble(info[21]),
+                        Double.parseDouble(info[22]));
                 courses.add(course);
             }
         } catch (IOException e) {
@@ -53,6 +70,11 @@ public class OnlineCoursesAnalyzer {
     }
 
     //1
+    /**
+     * This is a method description that is long enough to exceed right margin.
+     * Line with manual
+     * line feed.
+     */
     public Map<String, Integer> getPtcpCountByInst() {
         Map<String, Integer> map = new LinkedHashMap<>();
         List<Course> courses = this.courses;
@@ -69,6 +91,11 @@ public class OnlineCoursesAnalyzer {
     }
 
     //2
+    /**
+     * This is a method description that is long enough to exceed right margin.
+     * Line with manual
+     * line feed.
+     */
     public Map<String, Integer> getPtcpCountByInstAndSubject() {
         Map<String, Integer> map = new HashMap<>();
         List<Course> list = this.courses;
@@ -89,6 +116,11 @@ public class OnlineCoursesAnalyzer {
     }
 
     //3
+    /**
+     * This is a method description that is long enough to exceed right margin.
+     * Line with manual
+     * line feed.
+     */
     public Map<String, List<List<String>>> getCourseListOfInstructor() {
         Map<String, List<List<String>>> map = new HashMap<>();
         this.courses.forEach(course -> {
@@ -113,7 +145,11 @@ public class OnlineCoursesAnalyzer {
         });
         return map;
     }
-
+    /**
+     * This is a method description that is long enough to exceed right margin.
+     * Line with manual
+     * line feed.
+     */
     public static void getCourseListOfInstructorHelp(Map<String, List<List<String>>> map, String teacher, String title, int index) {
         teacher = teacher.strip();
         if (map.containsKey(teacher)) {
@@ -132,6 +168,11 @@ public class OnlineCoursesAnalyzer {
     }
 
     //4
+    /**
+     * This is a method description that is long enough to exceed right margin.
+     * Line with manual
+     * line feed.
+     */
     public List<String> getCourses(int topK, String by) {
         List<String> list = new ArrayList<>();
         HashSet<String> set = new HashSet<>();
@@ -153,6 +194,11 @@ public class OnlineCoursesAnalyzer {
     }
 
     //5
+    /**
+     * This is a method description that is long enough to exceed right margin.
+     * Line with manual
+     * line feed.
+     */
     public List<String> searchCourses(String courseSubject, double percentAudited, double totalCourseHours) {
         HashSet<String> set = new HashSet<>();
 
@@ -165,6 +211,11 @@ public class OnlineCoursesAnalyzer {
     }
 
     //6
+    /**
+     * This is a method description that is long enough to exceed right margin.
+     * Line with manual
+     * line feed.
+     */
     public List<String> recommendCourses(int age, int gender, int isBachelorOrHigher) {
         List<String> list;
         Map<String, Q6> map = new HashMap<>();
@@ -183,7 +234,7 @@ public class OnlineCoursesAnalyzer {
                 map.put(course.number, new Q6(course.percentMale, course.medianAge, course.percentDegree, 1, course.title, course.launchDate));
             }
         });
-        Comparator<Q6> comparator1 = Comparator.comparingDouble(a -> (Math.pow(age - a.age, 2) + Math.pow(gender*100 - a.male, 2) + Math.pow(isBachelorOrHigher*100
+        Comparator<Q6> comparator1 = Comparator.comparingDouble(a -> (Math.pow(age - a.age, 2) + Math.pow(gender * 100 - a.male, 2) + Math.pow(isBachelorOrHigher * 100
                 - a.degree, 2)));
         Comparator<Q6> comparator2 = Comparator.comparing(q6 -> q6.title);
         Comparator<Q6> comparator = comparator1.thenComparing(comparator2);
